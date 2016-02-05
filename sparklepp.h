937895a341e1e030d4eda80085e4f0ed541b2ca4
Steve Flower <steve.flower@audient.com>
2016-02-05 15:40:36 +0000

    Significant refactoring
    * This is done to enable extra functionality initially on mac
    * Windows support currently broken

--- sparklepp.h
+++ sparklepp.h
@@ -18,11 +18,37 @@ public:
     Sparkle(const juce::URL& appcastUrl);
     ~Sparkle();
     
-    void checkForUpdates();
+    /* This will asynchronously launch an update GUI if an update is available */
+    void checkForUpdateInBackground();
     
+    /* This will asynchronously check if an update is available.
+     * If an update is available the didFindValidUpdate method will becalled on 
+     * listeners.
+     * If an update is not available the updaterDidNotFindUpdate method will 
+     * be called on listeners.
+     */
+    void checkForUpdateInformation();
+    
+    class Listener
+    {
+    public:
+        virtual ~Listener() {}
+        
+        virtual void didFindValidUpdate() {}
+        virtual void updaterDidNotFindUpdate() {}
+    };
+    
+    void addListener (Listener* listener);
+    void removeListener (Listener* listener);
+    
+    /* internal */
+    void didFindValidUpdate();
+    void updaterDidNotFindUpdate();
 private:
     class Private;
     juce::ScopedPointer<Private> d;
+    
+    juce::ListenerList<Listener> listeners;
 };
 
 #endif
--- sparklepp.mm
+++ sparklepp.mm
@@ -12,11 +12,66 @@
 
 #include "sparklepp.h"
 
+@interface SparkleDelegate : NSObject <SUUpdaterDelegate> {
+    Sparkle* delegateHandler;
+}
+
+- (id) init:(Sparkle*)handler;
+
+/*!
+ Called when a valid update is found by the update driver.
+ 
+ \param updater The SUUpdater instance.
+ \param item The appcast item corresponding to the update that is proposed to be installed.
+ */
+- (void)updater:(SUUpdater *)updater didFindValidUpdate:(SUAppcastItem *)item;
+
+/*!
+ Called when a valid update is not found.
+ 
+ \param updater The SUUpdater instance.
+ */
+- (void)updaterDidNotFindUpdate:(SUUpdater *)updater;
+
+@end
+
+@implementation SparkleDelegate
+- (id) init:(Sparkle*)handler
+{
+    self = [super init];
+    if (self) {
+        delegateHandler = handler;
+    }
+    return self;
+}
+
+/*!
+ Called when a valid update is found by the update driver.
+ 
+ \param updater The SUUpdater instance.
+ \param item The appcast item corresponding to the update that is proposed to be installed.
+ */
+- (void)updater:(SUUpdater *)updater didFindValidUpdate:(SUAppcastItem *)item
+{
+    delegateHandler->didFindValidUpdate();
+}
+
+/*!
+ Called when a valid update is not found.
+ 
+ \param updater The SUUpdater instance.
+ */
+- (void)updaterDidNotFindUpdate:(SUUpdater *)updater
+{
+    delegateHandler->updaterDidNotFindUpdate();
+}
+@end
+
 class Sparkle::Private {
 public:
     SUUpdater* updater;
+    SparkleDelegate* delegateHandler;
 };
-
 
 Sparkle::Sparkle (const juce::URL& appcastURL)
 {
@@ -25,6 +80,8 @@ Sparkle::Sparkle (const juce::URL& appcastURL)
     NSURL* url = [NSURL URLWithString:
                   [NSString stringWithUTF8String: appcastURL.toString(true).toRawUTF8()]];
     [d->updater setFeedURL: url];
+    
+    [d->updater setDelegate: [[SparkleDelegate alloc] init: this]];
 }
 
 Sparkle::~Sparkle()
@@ -32,7 +89,32 @@ Sparkle::~Sparkle()
     [d->updater release];
 }
 
-void Sparkle::checkForUpdates()
+void Sparkle::checkForUpdateInBackground()
 {
     [d->updater checkForUpdatesInBackground];
+}
+
+void Sparkle::checkForUpdateInformation()
+{
+    [d->updater checkForUpdateInformation];
+}
+
+void Sparkle::didFindValidUpdate()
+{
+    listeners.call (&Listener::didFindValidUpdate);
+}
+
+void Sparkle::updaterDidNotFindUpdate()
+{
+    listeners.call (&Listener::updaterDidNotFindUpdate);
+}
+
+void Sparkle::addListener (Listener* listener)
+{
+    listeners.add (listener);
+}
+
+void Sparkle::removeListener (Listener* listener)
+{
+    listeners.remove (listener);
 }
